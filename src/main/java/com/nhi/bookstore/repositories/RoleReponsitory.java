0f35d3e73aa6a354ae7635f226380122546c40bc
nhi <nguyenthinhi0602@gmail.com>
2019-07-01 11:40:15 +0700

    edit1

--- src/main/java/com/nhi/bookstore/repositories/RoleReponsitory.java
+++
@@ -1,8 +0,0 @@
-package com.nhi.bookstore.repositories;
-
-import com.nhi.bookstore.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleReponsitory extends JpaRepository<Role, Integer> {
-    Role findByNameContaining(String name);
-}
